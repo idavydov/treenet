89a8b33ba11194034a872165818284064d25082e
Iakov Davydov <iakov.davydov@unil.ch>
2016-12-07 15:31:12 +0100

    don't deroot the tree

--- src/m0_trees.sh
+++ src/m0_trees.sh
@@ -10,7 +10,7 @@ do
 	echo $fn
 	ot=data/m0/$fn.onwk
 	t=${ot%.*}.nwk
-	$DNDSTOOLSPATH/gen_tree.py --ntax $nseq --birth-rate 0.3 --death-rate 0.1 > $ot
+	$DNDSTOOLSPATH/gen_tree.py --no-deroot --ntax $nseq --birth-rate 0.3 --death-rate 0.1 > $ot
 	$DNDSTOOLSPATH/scale_tree.py $ot $tlen $t
 	$DNDSTOOLSPATH/label_internal_nodes.py --in-place $t
 done
